b84a51e6560a5235e8b437de4d0b0d0aeed514f6
aeriihia <89082133+aeriihia@users.noreply.github.com>
2023-03-31 00:16:09 +0300

    13.4

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.static('build'))
 
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -19,17 +19,66 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+class Blog extends Model {}
 
-main()
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(blogs.map(b => b.toJSON()))
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    console.log(blog.toJSON())
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  console.log(blog.toJSON())
+  if (blog) {
+    try {
+      await Blog.destroy({where:{id: req.params.id}})
+      return res.status(200).json('OK')
+    } catch (error) {
+      return res.status(400).json({ error })
+    }
+  }
+})
 
 const PORT = 3003
 app.listen(PORT, () => {
