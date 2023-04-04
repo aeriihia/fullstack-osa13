08e2867684d701a1b3d962e0612d3f3df11eb253
aeriihia <89082133+aeriihia@users.noreply.github.com>
2023-04-05 02:09:06 +0300

    13.7

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -1,43 +1,44 @@
 const router = require('express').Router()
+require('express-async-errors')
 
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name) {
+    return res.status(400).send({ error: error.name })
+  }
+
+  next(error)
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
-  console.log(blogs.map(b => b.toJSON()))
   res.json(blogs)
 })
 
 router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    console.log(blog.toJSON())
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  const blog = await Blog.create(req.body)
+  res.json(blog)
 })
 
-router.put('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  console.log(blog.toJSON())
-  if (blog) {
-    blog.likes = req.body.likes
-    await blog.save()
-    console.log(blog.toJSON())
-    return res.json(blog)
-  } else {
-    res.status(404).end()
-  }
+router.put('/:id', blogFinder, async (req, res) => {
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  res.json(req.blog)
 })
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  console.log(blog.toJSON())
-  if (blog) {
-    await blog.destroy()
-  }
+router.delete('/:id', blogFinder, async (req, res) => {
+  await req.blog.destroy()
   res.status(204).end()
 })
+
+router.use(errorHandler)
 
 module.exports = router
