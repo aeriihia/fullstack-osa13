78677388cebc16d27ddce0f93aededdec8b9fbfa
aeriihia <89082133+aeriihia@users.noreply.github.com>
2023-04-18 00:59:39 +0300

    13.9

--- bloglist-backend/controllers/users.js
+++ bloglist-backend/controllers/users.js
@@ -1,6 +1,17 @@
 const router = require('express').Router()
+require('express-async-errors')
 
 const { User } = require('../models')
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error) {
+    return res.status(400).send({ "error": [ "Validation isEmail on username failed" ] })
+  }
+
+  next(error)
+}
 
 router.get('/', async (req, res) => {
   const users = await User.findAll()
@@ -8,12 +19,8 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  try {
-    const user = await User.create(req.body)
-    res.json(user)
-  } catch(error) {
-    return res.status(400).json({ error })
-  }
+  const user = await User.create(req.body)
+  res.json(user)
 })
 
 router.put('/:username', async (req, res) => {
@@ -26,5 +33,7 @@ router.put('/:username', async (req, res) => {
     res.status(404).end()
   }
 })
+
+router.use(errorHandler)
 
 module.exports = router
